1d44573f36bdc603cf79039312610e662d1e71a5
Nate Moore <nate@skypack.dev>
2022-04-25 15:23:47 -0400

    fix cjs issue

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,5 @@
-import title from 'smartypants';
-
 export const show = {
-    title: title(`Let's build with Astro`),
+    title: `Let's build with Astro`,
     host: {
         name: "Nate Moore",
         twitter: 'n_moore',
